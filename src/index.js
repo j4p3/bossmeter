5c3c88e7509c356eab4c55a509ca195d709b6a33
JP <bonnerjp@gmail.com>
2017-01-22 15:22:29 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ let app = express();
 
 app.server = http.createServer(app)
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.json())
 
 // 3rd party middleware
